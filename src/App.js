fe937cf0333db1f124fa2b891db64b67bbdbe0f5
Naved Anjum Rehman <navedar@gmail.com>
2020-08-26 14:47:53 +0530

    Added Switch & Routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Components/Login";
-import { BrowserRouter} from 'react-router-dom';
+import DashBoard from "./Components/DashBoard";
+
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <BrowserRouter>
-    <Login />
-    
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/DashBoard" component={DashBoard}/>
+      </Switch>
     </BrowserRouter>
-      
-     
+
+
   );
 }
 
